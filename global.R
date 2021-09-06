f8e4b640dee099ca9dffe4e1fa5bbd7cbfd84d7c
Epi-interactive <info@epi-interactive.com>
2021-09-06 15:32:33 +1200

    Update global.R

--- global.R
+++ global.R
@@ -1,3 +1,9 @@
+##################################
+# Created by EPI-interactive
+# 17 May 2019
+# https://www.epi-interactive.com
+##################################
+
 library(shiny)
 library(shinyrouter)
 library(stringr)
