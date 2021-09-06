1a6bcbb761bb70f9be80a3d11f10a2bc098ef041
Epi-interactive <info@epi-interactive.com>
2021-09-06 15:33:22 +1200

    Update splash.R

--- source/pages/splash.R
+++ source/pages/splash.R
@@ -1,3 +1,9 @@
+##################################
+# Created by EPI-interactive
+# 17 May 2019
+# https://www.epi-interactive.com
+##################################
+
 pageSplashUI <- function(id) {
  div(class="main",
      div(class="splash-container", 
